6193514f56a3faa726128c994568fccb980c1033
johunt89 <79724849+johunt89@users.noreply.github.com>
2023-05-09 21:06:26 -0400

    active tabs added

--- my-project/src/canvas/index.jsx
+++ my-project/src/canvas/index.jsx
@@ -6,14 +6,20 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5}/>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
       <Environment preset="city" />
+
       <CameraRig>
-      {/*  <Backdrop/> */}
-        <Center>
-          <Shirt />
-        </Center>
+        <Backdrop />
+          <Center>
+            <Shirt />
+          </Center>
       </CameraRig>
     </Canvas>
   )
